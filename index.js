8493ba05e91abdb936a710173cbc2c0ff1552811
Chung <chung@lakoo.com>
2018-06-14 15:56:27 +0800

    Improve UI

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ const app = new Vue({
         address: '',
         proposals: [],
         isHost: false,
+        isParticipant: false,
         description: '',
+    },
+    computed: {
+        identity: function() {
+            if (this.isHost) {
+                return 'Teacher';
+            } else if (this.isParticipant) {
+                return 'Student';
+            }
+            return 'Who are you?';
+        }
     },
     methods: {
         async refresh() {
@@ -22,16 +33,13 @@ const app = new Vue({
             const isParticipant = await Vote.methods.isParticipant(address).call();
             for (let i = 0; i < proposalCount; i += 1) {
                 const proposal = await Vote.methods.proposals(i).call();
-                if (isParticipant) {
-                    proposal.canVote = !(await Vote.methods.alreadyVoted(i, address).call());
-                } else {
-                    proposal.canVote = false;
-                }
+                proposal.alreadyVoted = await Vote.methods.alreadyVoted(i, address).call();
                 proposals.push(proposal);
             }
             this.address = address;
             this.proposals = proposals;
             this.isHost = isHost;
+            this.isParticipant = isParticipant;
         },
         async vote(id, agree) {
             await Vote.methods.vote(id, agree).send({ from: this.address });
